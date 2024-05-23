434bdaf206b3cf01011c5b6df299a4bd5615c19f
DecadeXD <105734719+sevycanh@users.noreply.github.com>
2024-05-24 01:03:02 +0700

    Update bookmark.js

--- routes/bookmark.js
+++ routes/bookmark.js
@@ -3,7 +3,7 @@ const bookmarkController = require("../controllers/bookmarkController");
 const { verifyAndAuthorization } = require("../middleware/verifyToken");
 
 // CREATE BOOKMARKS
-router.post("/", bookmarkController.createBookmark);
+router.post("/", verifyTokenAndAuthorization, bookmarkController.createBookmark);
 
 // DELETE BOOKMARKS
 router.delete("/:id", bookmarkController.deleteBookmark);
